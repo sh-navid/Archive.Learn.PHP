8cffbfa8b0882776387f2bc33b793e1c71f7e3e9
sh-navid <shamshirsaz.navid@live.com>
2022-10-25 23:12:56 +0330

    Include iterable datatype

---
+++ php/concepts/datatypes/datatype-callable.php
@@ -0,0 +1,18 @@
+<?php
+
+//Callable, Callback
+
+function f(){
+    return "Hello";
+}
+
+function f2(callable $callback){
+    echo var_dump($callback);
+    //string(1) "f"
+    echo gettype($callback),"\n";
+    // string
+    echo $callback();
+    // Hello
+}
+
+f2(f);
---
+++ php/concepts/datatypes/datatype-iterable.php
@@ -0,0 +1,22 @@
+<?php
+
+function Generator():iterable{
+    yield "10%";
+    yield "50%";
+    yield "100%";
+}
+
+foreach(Generator() as $v){
+    echo $v."\n";
+}
+
+
+
+function func(iterable $arr=[]):iterable{
+    $arr[0]*=200;
+    return $arr;
+}
+
+foreach(func([10,20,30]) as $v){
+    echo $v."\n";
+}
