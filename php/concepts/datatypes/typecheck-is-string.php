55e5ef5f4e7c5baf6a1c5a227e4f3779fcdb9fe4
sh-navid <shamshirsaz.navid@live.com>
2022-10-26 00:09:38 +0330

    Update type check

---
+++ php/concepts/datatypes/typecheck-is-string.php
@@ -0,0 +1,14 @@
+<?php
+
+$i="AA";
+
+$check=is_string($i);
+
+echo "<",$check,">","\n";
+echo gettype($check),"\n";
+
+if($check){
+    echo "YES";
+}else{
+    echo "NO";
+}
---
+++ php/concepts/datatypes/typecheck-set-type.php
@@ -0,0 +1,9 @@
+<?php
+
+$i=12;
+echo $i," ",gettype($i),"\n";
+//12 integer
+
+settype($i,"float");
+echo $i," ",gettype($i);
+//12 double
