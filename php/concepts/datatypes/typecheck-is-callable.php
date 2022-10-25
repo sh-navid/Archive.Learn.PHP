fc34ebb0a6dbc0b42b222bc39494e3e2498f9b43
sh-navid <shamshirsaz.navid@live.com>
2022-10-26 00:36:51 +0330

    Check callable

---
+++ php/concepts/datatypes/typecheck-is-callable.php
@@ -0,0 +1,18 @@
+<?php
+
+//Callable, Callback
+
+function f()
+{
+    return "Hello";
+}
+
+function f2(callable $callback)
+{
+    echo var_dump($callback);
+    echo (is_callable($callback) ? "True" : "False"), "\n";
+    // True
+    echo $callback();
+}
+
+f2(f);
