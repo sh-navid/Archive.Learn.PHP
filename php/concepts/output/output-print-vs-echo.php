64af6e650883256e6262ad353b1b2acabd329574
sh-navid <shamshirsaz.navid@live.com>
2022-10-25 22:02:03 +0330

    Update var_dump

---
+++ php/concepts/output/output-print-vs-echo.php
@@ -0,0 +1,4 @@
+<?php
+
+# print 1,2,3,"\n"; // This is not working
+echo 1,2,3,"\n";
--- php/concepts/output/output-var-dump.php
+++ php/concepts/output/output-var-dump.php
@@ -1,1 +1,12 @@
 <?php
+
+$a = 12000000;
+$b = "hello";
+
+$d = [4,5,6];
+$e = array(4=>10,5=>20,"H"=>30);
+
+var_dump($a);
+var_dump($b);
+var_dump($d);
+var_dump($e);
