692ed0255efa8efd7176bcf324fedb81b8fff51c
sh-navid <shamshirsaz.navid@live.com>
2023-01-06 04:46:45 +0330

    Example 01

---
+++ php/examples/example-01.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Example 01</title>
+</head>
+
+<body>
+    <h1>Home Page</h1>
+    <?php for ($i = 0; $i < 10; $i++) echo '<span>' . $i . '</span>' ?>
+
+    <?php for ($i = 0; $i < 10; $i++) : ?>
+        <div>
+            <h4><?= $i ?></h4>
+        </div>
+    <?php endfor ?>
+
+    <?php
+    $data = [
+        ["Title" => "Book 01", "Price" => 1000],
+        ["Title" => "Book 02", "Price" => 2000],
+        ["Title" => "Book 03", "Price" => 3000],
+    ]
+    ?>
+</body>
+
+</html>
