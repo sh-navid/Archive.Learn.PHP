7f90575dd9903dac841a86e42176eebb13a20c32
sh-navid <shamshirsaz.navid@live.com>
2023-01-06 05:20:46 +0330

    Update CSR Example

--- php/examples/example-02.php
+++ php/examples/example-02.php
@@ -1,7 +1,14 @@
 <?php
-if ($_GET['data'] == true) {
-    $A = [10, 12, 14, 16, 18];
-    echo implode(",", $A);
+if ($_GET['data'] == 'array') {
+    $data = [10, 12, 14, 16, 18];
+    echo implode(",", $data);
+} else if ($_GET['data'] == 'json') {
+    $data = [
+        ["Title" => "Book 01", "Price" => 1000],
+        ["Title" => "Book 02", "Price" => 2000],
+        ["Title" => "Book 03", "Price" => 3000],
+    ];
+    echo json_encode($data);
 } else {
     echo readfile("example-02.html")[0];
 }
