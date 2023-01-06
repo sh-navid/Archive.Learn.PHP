aa31a28beb995e990a57e3fc7fcf9da4d51bb1ef
sh-navid <shamshirsaz.navid@live.com>
2023-01-06 07:55:39 +0330

    File uploader script

--- php/examples/example-03.php
+++ php/examples/example-03.php
@@ -1,11 +1,13 @@
 <?php
 if ($_GET['upload'] == true) {
-    $file = "uploads/" . basename($_FILES["myfile"]["name"]);
+    if (!empty($_FILES['myfile'])) {
+        $file_path = "media/";
+        $file_path = $file_path . basename($_FILES['myfile']['name']);
 
-    if (move_uploaded_file($_FILES["myfile"]["name"], $file)) {
-        echo "Upload complete";
-    } else {
-        echo "Something is wrong";
+        if (move_uploaded_file($_FILES['myfile']['tmp_name'], $file_path))
+            echo "Uploaded";
+        else
+            echo "Something is wrong";
     }
 } else {
     echo readfile("example-03.html")[0];
