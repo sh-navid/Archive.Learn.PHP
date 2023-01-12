c5d407e14f4f679d91b7a9534573d1c56a2638a0
sh-navid <shamshirsaz.navid@live.com>
2023-01-12 07:07:46 +0330

    GD

--- php/concepts/gd/gd-image-filter.php
+++ php/concepts/gd/gd-image-filter.php
@@ -8,9 +8,14 @@ $color = imagecolorallocate($img, 30, 30, 90);
 // imagefilter($img, IMG_FILTER_PIXELATE, 10, 10);
 
 // Black and white image
-imagefilter($im, IMG_FILTER_GRAYSCALE);
-imagefilter($im, IMG_FILTER_CONTRAST, -90);
+imagefilter($img, IMG_FILTER_GRAYSCALE);
+imagefilter($img, IMG_FILTER_CONTRAST, -90);
 
 imagepng($img, __DIR__ . "/test-image.png");
 
 imagedestroy($img);
+
+
+
+// Helper links:
+// https://stackoverflow.com/questions/254388/how-do-you-convert-an-image-to-black-and-white-in-php
