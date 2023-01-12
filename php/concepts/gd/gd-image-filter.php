8ae188ccbe8b143a36a00e1c332d8a0816e05de7
sh-navid <shamshirsaz.navid@live.com>
2023-01-12 07:06:16 +0330

    GD

--- php/concepts/gd/gd-image-filter.php
+++ php/concepts/gd/gd-image-filter.php
@@ -4,8 +4,12 @@ $img = imagecreatefrompng(__DIR__ . "/test-image.png");
 
 $color = imagecolorallocate($img, 30, 30, 90);
 
-//imagefilter($img, IMG_FILTER_NEGATE);
-imagefilter($img, IMG_FILTER_PIXELATE, 10, 10);
+// imagefilter($img, IMG_FILTER_NEGATE);
+// imagefilter($img, IMG_FILTER_PIXELATE, 10, 10);
+
+// Black and white image
+imagefilter($im, IMG_FILTER_GRAYSCALE);
+imagefilter($im, IMG_FILTER_CONTRAST, -90);
 
 imagepng($img, __DIR__ . "/test-image.png");
 
