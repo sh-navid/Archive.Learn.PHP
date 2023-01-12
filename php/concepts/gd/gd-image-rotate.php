dd2e32d688276fdfcca4857bc00f5d9adc5281ad
sh-navid <shamshirsaz.navid@live.com>
2023-01-12 06:52:23 +0330

    GD

--- php/concepts/gd/gd-image-rotate.php
+++ php/concepts/gd/gd-image-rotate.php
@@ -4,7 +4,7 @@ $img = imagecreatefrompng(__DIR__ . "/test-image.png");
 
 $color = imagecolorallocate($img, 30, 30, 90);
 
-$img=imagerotate($img,45,$color,true);
+$img = imagerotate($img, 45, $color, true);
 
 imagepng($img, __DIR__ . "/test-image.png");
 
