ec7c7ac8ac66b843d5749680b644f0cf88309252
sh-navid <shamshirsaz.navid@live.com>
2023-01-12 07:18:42 +0330

    GD

--- php/concepts/gd/dg-image-set-pixel.php
+++ php/concepts/gd/dg-image-set-pixel.php
@@ -3,9 +3,9 @@
 $img = imagecreatefrompng(__DIR__ . "/test-image.png");
 
 
-for ($a = 0; $a < 20; $a += 8) {
-    for ($b = 0; $b < 20; $b += 8) {
-        $color = imagecolorallocate($img, $a+50, $b+30, $a + $b);
+for ($a = 0; $a < 200; $a += 5) {
+    for ($b = 0; $b < 200; $b += 5) {
+        $color = imagecolorallocate($img, $a + 50, $b + 30, $a + $b);
         imagesetpixel($img, $a, $b, $color);
     }
 }
