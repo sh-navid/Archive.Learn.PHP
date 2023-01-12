e59c9740a7689addd318d9b56942fa59be7fb335
sh-navid <shamshirsaz.navid@live.com>
2023-01-12 07:02:16 +0330

    GD

--- php/concepts/gd/gd-image-filter.php
+++ php/concepts/gd/gd-image-filter.php
@@ -5,7 +5,7 @@ $img = imagecreatefrompng(__DIR__ . "/test-image.png");
 $color = imagecolorallocate($img, 30, 30, 90);
 
 //imagefilter($img, IMG_FILTER_NEGATE);
-imagefilter($img, IMG_FILTER_PIXELATE);
+imagefilter($img, IMG_FILTER_PIXELATE, 10, 10);
 
 imagepng($img, __DIR__ . "/test-image.png");
 
